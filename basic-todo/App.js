251120979ab615822bdec97772a70744ae346b6c
a1nkur <ankur29mac@gmail.com>
2023-05-25 19:27:39 +0530

    basic-todo: improvinng the layout

---
+++ basic-todo/App.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1, // take all available space
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center", // if 'stretch' , button text would look off
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%", // always be sure of what width you are giving
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5, // take 5 fraction of space.s
+  },
+});

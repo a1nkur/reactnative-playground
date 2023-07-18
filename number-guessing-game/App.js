bd50b2b91c27cbc9ae96f8f266c616d5e8a8b80f
a1nkur <ankur29mac@gmail.com>
2023-07-18 14:07:27 +0530

    Added visual feedback in "number-guessing-game"

--- number-guessing-game/App.js
+++ number-guessing-game/App.js
@@ -1,9 +1,17 @@
-import { StyleSheet } from "react-native";
-
+import { StyleSheet, View } from "react-native";
 import GameStartScreen from "./screens/game-start-screen";
 
 export default function App() {
-  return <GameStartScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <GameStartScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
+});
